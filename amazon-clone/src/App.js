f69a1f9f986f521e47efd69540997ab5ad49830c
code-vibe <mrsamoladapo@gmail.com>
2022-02-05 18:03:04 +0100

    initial commit

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    //BEM naming convention
+    <div className="app">
+      <h>Hey!! Lets build the Amazon Store </h>
+    </div>
+  );
+}
+
+export default App;
+ 
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCV4SeXmdmbWpvLSc9DpHAnGyxEGuxH8qE",
+  authDomain: "clone-3aa6a.firebaseapp.com",
+  projectId: "clone-3aa6a",
+  storageBucket: "clone-3aa6a.appspot.com",
+  messagingSenderId: "279274309596",
+  appId: "1:279274309596:web:d39580a8811a5f7b51f119",
+  measurementId: "G-3DZ2PNF0KF"
+};
